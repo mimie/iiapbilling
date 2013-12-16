c8fe6603d992f2e1a338b82d340ce74843873d8d
mimie <karen.convicto@yahoo.com>
2013-12-16 19:17:30 +0800

    added pagination

--- membershipIndividualBilling.php
+++ membershipIndividualBilling.php
@@ -6,6 +6,21 @@
   <link rel="stylesheet" type="text/css" href="menu.css">
   <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
   <script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
+  <script src="js/jquery-jPaginate.js"></script>
+  <script src="js/jquery.tablesorter.js"></script>
+<script>
+$(function() {
+        $( "#tabs" ).tabs().addClass( "ui-tabs-vertical ui-helper-clearfix" );
+        $( "#tabs li" ).removeClass( "ui-corner-top" ).addClass( "ui-corner-left" );
+        $('table').jPaginate({
+                'max': 10,
+                'page': 1,
+                'links': 'buttons'
+        });
+//        $("table").tablesorter( {sortList: [[0,0], [1,0]]} ); 
+});
+
+</script>
 </head>
 <body>
 <?php
