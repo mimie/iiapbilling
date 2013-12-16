74961b1fbdf3769b6e63d9d72bd1f55464488d5d
mimie <karen.convicto@yahoo.com>
2013-12-16 14:30:58 +0800

    display all companies

--- membershipCompanyBilling.php
+++ membershipCompanyBilling.php
@@ -3,6 +3,7 @@
   <title>Membership Company Billing</title>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
   <link rel="stylesheet" type="text/css" href="billingStyle.css">
+  <link rel="stylesheet" type="text/css" href="menu.css">
 </head>
 <body>
 <?php
@@ -11,9 +12,25 @@
   include 'membership_functions.php';
 
   $dbh = civicrmConnect();
+  
+  session_start();
+  //if the user has not logged in
+  if(!isLoggedIn())
+  {
+    header('Location: login.php');
+    die();
+  }
+
+  @$userId = $_GET["user"];
+
+  $logout = logoutDiv($dbh,$userId);
+  echo $logout;
+  $header = headerDiv();
+  echo $header;
   $companies = getAllCompanies($dbh);
 
-
+  $displayCompanies = displayAllCompanies($dbh,$companies);
+  echo $displayCompanies;
 ?>
 </body>
 </html>
